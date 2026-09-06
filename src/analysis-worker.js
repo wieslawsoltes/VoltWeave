566c0da045ffa019c7adefc84efda3cf034a3368
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-09-06 22:04:29 +0200

    deploy: publish tested VoltWeave application and engineering examples

--- src/analysis-worker.js
+++
@@ -1,6 +0,0 @@
-import {analyzeProject} from './graph.js';
-self.onmessage=({data})=>{
-  const start=performance.now();
-  try {self.postMessage({revision:data.revision,result:analyzeProject(data.project),elapsed:performance.now()-start});}
-  catch(error){self.postMessage({revision:data.revision,error:error.message});}
-};
